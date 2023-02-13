0e876a595432d198ac75155c48658b67c09c08fc
Alexyaaaaa <122922093+Alexyaaaaa@users.noreply.github.com>
2023-02-13 14:24:25 -0500

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -22,4 +22,24 @@ def on_on_update():
         PianoAQueue
     """))
     piano4.set_position(74, 78)
+    tiles.set_current_tilemap(tilemap("""
+        niveau3
+    """))
+    tileUtil.cover_all_tiles(assets.tile("""
+        myTile0
+    """), sprites.castle.tile_path5)
+    tileUtil.cover_all_tiles(assets.tile("""
+        myTile4
+    """), sprites.castle.tile_grass3)
+    tileUtil.cover_all_tiles(assets.tile("""
+            waterBorder
+        """),
+        sprites.castle.tile_grass1)
+    tileUtil.create_sprites_on_tiles(assets.tile("""
+            myTile3
+        """),
+        assets.image("""
+            forestTree1
+        """),
+        SpriteKind.Sapin1)
 game.on_update(on_on_update)
--- main.ts
+++ main.ts
@@ -5,16 +5,4 @@ namespace SpriteKind {
     export const PianoAQueue = SpriteKind.create()
     export const Sapin1 = SpriteKind.create()
 }
-let piano4: Sprite = null
-game.onUpdate(function () {
-    tiles.setCurrentTilemap(tilemap`niveau2`)
-    scene.setBackgroundImage(assets.image`stage`)
-    piano4 = sprites.create(assets.image`PianoAQueue`, SpriteKind.Enemy)
-    piano4.setImage(assets.image`PianoAQueue`)
-    piano4.setPosition(74, 78)
-    tiles.setCurrentTilemap(tilemap`niveau3`)
-    tileUtil.coverAllTiles(assets.tile`myTile0`, sprites.castle.tilePath5)
-    tileUtil.coverAllTiles(assets.tile`myTile4`, sprites.castle.tileGrass3)
-    tileUtil.coverAllTiles(assets.tile`waterBorder`, sprites.castle.tileGrass1)
-    tileUtil.createSpritesOnTiles(assets.tile`myTile3`, assets.image`forestTree1`, SpriteKind.Sapin1)
-})
+scene.setBackgroundImage(assets.image`stage`)
